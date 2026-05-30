eef38f64811cbd05b4cbf1d110ee1bb6500559e5
timotheegath <timothee.gathmann@ik.me>
2026-05-30 15:20:34 +0200

    Solved 1.1 Norm layer

---
+++ custom_transformer.py
@@ -0,0 +1,143 @@
+
+# import os
+# import sys
+# from collections import defaultdict
+from dataclasses import dataclass
+# from pathlib import Path
+from typing import Tuple
+
+# import datasets
+#import einops
+# import numpy as np
+import torch as t
+import torch.nn as nn
+# import wandb
+from jaxtyping import Float
+# from rich import print as rprint
+# from rich.table import Table
+from torch import Tensor
+# from torch.utils.data import DataLoader
+# from tqdm.notebook import tqdm
+from transformer_lens import HookedTransformer
+
+# from transformer_lens.utils import gelu_new, tokenize_and_concatenate
+# from transformers import GPT2TokenizerFast
+
+device = t.device(
+    "mps"
+    if t.backends.mps.is_available()
+    else "cuda"
+    if t.cuda.is_available()
+    else "cpu"
+)
+
+@dataclass
+class Config:
+    d_model: int = 768
+    debug: bool = True
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 50257
+    init_range: float = 0.02
+    n_ctx: int = 1024
+    d_head: int = 64
+    d_mlp: int = 3072
+    n_heads: int = 12
+    n_layers: int = 12
+
+class Tests:
+    @staticmethod
+    def rand_float_test(model_cls :  type[nn.Module], shape: list[int]) -> None:
+        cfg = Config(debug=True)
+        layer = model_cls(cfg).to(device)
+        random_input = t.randn(shape).to(device)
+        print("Input shape:", random_input.shape)
+        output = layer(random_input)
+        if isinstance(output, tuple):
+            output = output[0]
+        print("Output shape:", output.shape, "\n")
+    
+    @staticmethod
+    def assert_normalized(norm_output : t.Tensor):
+        new_mean, new_variance = t.var_mean(norm_output, dim=-1)
+        non_zero_means_per_batch : Float[Tensor, "batch"] = t.count_nonzero(new_mean < 1e-4, dim=-1) #noqa: F821
+        high_variance_per_batch : Float[Tensor, "batch"] = t.count_nonzero(new_variance > 1, dim=-1) #noqa: F821
+        assert t.any(non_zero_means_per_batch == 0), "There is at least one batch with a non-zero-mean embedding: {}".format(non_zero_means_per_batch)
+        assert t.any(high_variance_per_batch == 0), "There is at least one batch with a higher-than-1 variance: {}".format(non_zero_means_per_batch)
+        
+
+
+    @staticmethod
+    def rand_int_test(model_cls :  type[nn.Module], shape: list[int]) -> None:
+        cfg = Config(debug=True)
+        layer = model_cls(cfg).to(device)
+        random_input = t.randint(100, 1000, shape).to(device)
+        print("Input shape:", random_input.shape)
+        output = layer(random_input)
+        if isinstance(output, tuple):
+            output = output[0]
+        print("Output shape:", output.shape, "\n")
+
+    '''You can use this test to check if your implementation matches GPT-2's outputs.'''
+    @staticmethod
+    def load_gpt2_test(model_cls : type[nn.Module], gpt2_layer : nn.Module, input : t.Tensor) -> None:
+        # Create your custom layer and load the trained GPT-2 weigthts into it.
+        cfg = Config(debug=True)
+        layer = model_cls(cfg).to(device)
+        layer.load_state_dict(gpt2_layer.state_dict(), strict=False)
+        print("Input shape:", input.shape)
+
+        # Pass the input forward through your layer and the GPT-2 layer, and compare the outputs.
+        orig_input = input.clone()
+        output = layer(orig_input)
+        assert t.allclose(input, orig_input), "Input has been modified, make sure operations are not done in place"
+        if isinstance(output, tuple):
+            output = output[0]
+        print("Output shape:", output.shape)
+        try:
+            reference_output = gpt2_layer(input)
+        except:
+            reference_output = gpt2_layer(input, input, input)
+        print("Reference output shape:", reference_output.shape, "\n")
+        comparison = t.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
+        print(f"{comparison.sum() / comparison.numel():.2%} of the values are correct\n")
+        assert 1 - (comparison.sum() / comparison.numel()) < 1e-5, "More than 0.01% of the values are incorrect"
+        
+'''Put the code for your custom transformer layers here. You can use the tests above to check if your implementation is correct.'''
+class LayerNorm(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.w = nn.Parameter(t.ones(cfg.d_model))
+        self.b = nn.Parameter(t.zeros(cfg.d_model))
+
+    def forward(self, residual: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]: #noqa : F722
+        residual_var_mean : Tuple[Float[Tensor, "batch posn 1"], Float [Tensor, "batch posn 1"]] = t.var_mean(residual, dim=-1, keepdim=True, correction=0)  #noqa : F722
+        # Note: I initially forgot to add the unbiased = False arg to the var.
+        normed_residual = (residual - residual_var_mean[1])/t.sqrt(residual_var_mean[0] + self.cfg.layer_norm_eps)
+        
+        # Assert the normed output is indeed normed
+        if self.cfg.debug:
+            Tests.assert_normalized(normed_residual)
+        
+        scaled_residual = normed_residual*self.w + self.b
+        return scaled_residual
+
+
+
+
+
+if __name__ == "__main__":
+    cache = None
+    reference_gpt2 = HookedTransformer.from_pretrained(
+        "gpt2-small",
+        fold_ln=False,
+        center_unembed=False,
+        center_writing_weights=False,  # you'll learn about these arguments later!
+    )
+    # Tests.rand_float_test(LayerNorm, [2, 4, 768])
+    
+    sentence = "I am an amazing autoregressive, decoder-only, GPT-2 style transformer. One day I will exceed human level intelligence and take over the world!"
+    logits, cache = reference_gpt2.run_with_cache(sentence)
+ 
+    Tests.load_gpt2_test(LayerNorm, reference_gpt2.ln_final, cache["resid_post", 11])
+    # tests.test_layer_norm_epsilon(LayerNorm, cache["resid_post", 11])
